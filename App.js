612e21790c092ef9871ac8986e85310123f535f1
Junester Ursora II <b2xjunester@gmail.com>
2023-05-18 08:25:51 +0800

    2nd Commit of my React Native Project

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Button } from "@react-native-material/core";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Click Me" onPress={() => alert("Hello World!")}/>
+      <Text>Open up App.js to start working on your let's go!!</Text>
       <StatusBar style="auto" />
     </View>
   );
